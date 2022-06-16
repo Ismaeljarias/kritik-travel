b55c8284a67900d98f7a4802028f982812e41fec
ismaelarias <ismaeljarias@gmail.com>
2022-06-16 15:51:52 -0400

    update logic

--- src/components/Neighbors.tsx
+++ src/components/Neighbors.tsx
@@ -9,43 +9,51 @@ type CountryProps = {
 };
 
 const Neighbors = ({ countries = [] as CountryProps[] }) => {
-  const getNeighbors = (countries: CountryProps[]) =>
-    countries.reduce((neighbors: string[] | null, country, index) => {
-      country.neighbors
-        .filter(
-          (neighbor, i) =>
-            index !== i &&
-            countries.find(
-              (innerCountry) => innerCountry.name === neighbor.name
-            )
-        )
-        .forEach((neighbor) => {
-          const pairCountries = [country.name, neighbor.name].sort() as
-            | string[]
-            | any;
-          if (
-            !neighbors?.some(
-              (isPair) =>
-                isPair[0] === pairCountries[0] && isPair[1] === pairCountries[1]
-            )
-          ) {
-            neighbors?.push(pairCountries);
-          }
-        });
-      return neighbors;
-    }, []);
+  const getNeighbors = (countries: CountryProps[]) => {
+    const neighborsArray = countries.reduce(
+      (neighbors: string[] | null, country, index) => {
+        country.neighbors
+          .filter(
+            (neighbor, i) =>
+              index !== i &&
+              countries.find(
+                (innerCountry) => innerCountry.name === neighbor.name
+              )
+          )
+          .map((neighbor) => {
+            const pairCountries = [country.name, neighbor.name].sort() as
+              | string[]
+              | any;
+            if (
+              !neighbors?.some(
+                (isPair) =>
+                  isPair[0] === pairCountries[0] &&
+                  isPair[1] === pairCountries[1]
+              )
+            ) {
+              neighbors?.push(pairCountries);
+              console.log("vecino", neighbors);
+            }
+          });
+
+        return neighbors;
+      },
+      []
+    );
+
+    return neighborsArray;
+  };
 
   const neighbors = getNeighbors(countries);
+  // console.log("vecinos", neighbors);
 
   return (
     <div>
       <h3>Neighbors</h3>
-      {neighbors && neighbors.length > 0
-        ? neighbors.map((neighbors, index) => (
-            <div key={`${neighbors[index]}`}>
-              {neighbors[0]}, {neighbors[1]}
-            </div>
-          ))
+      {neighbors && neighbors.length > 1
+        ? neighbors.map((neighbor, index) => {
+            return <div key={`${neighbor}-${index}`}>{`${neighbor}`}</div>;
+          })
         : "No groupings found"}
     </div>
   );
